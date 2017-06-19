8f91de05f574bb37611a5814209862338817244c
alien-house <info@alien-house.com>
2017-06-19 16:21:40 -0700

    add getting database

--- Note/src/main/java/LoginModel.java
+++ Note/src/main/java/LoginModel.java
@@ -15,7 +15,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.tasks.OnCompleteListener;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  *
@@ -24,22 +29,67 @@ import java.io.FileNotFoundException;
 public class LoginModel {
     
     DatabaseReference ref;
-    
+    LinkedHashMap<String, String> userList;
+            
     public LoginModel() throws FileNotFoundException{
-        ref = NoteGlobals.getUserReference();
-        
-        
         UserAccount user = new UserAccount("komagire@gmaicom","papapa");
         Gson gson = new Gson();
         String text = gson.toJson(user);
         
-        System.out.println(text);        // 「hello」と出力
+        ref = NoteGlobals.getUserReference();
+        
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getValue().getClass());
+                Gson gsons = new Gson();
+                Map<String, Object> map1 = new HashMap<String, Object>((Map<? extends String, ? extends Object>) dataSnapshot.getValue()); 
+//                HashMap<String, Object> hashmap = dataSnapshot.getValue();
+//                HashMap<String, Object> hashmap = new HashMap<String, Object>( dataSnapshot.getValue());
+//                Map<String, Object> map = new HashMap( dataSnapshot.getValue());
+
+                System.out.println("user number"+ map1.size());
+                map1.forEach((x,y)-> {
+//                    System.out.println("key : " + x + " , value : " + y);
+//                    System.out.println(y.getClass());
+
+                    userList = new LinkedHashMap<String, String>( (Map<? extends String, ? extends String>) y);
+                    userList.forEach((x2,y2)-> {
+                        System.out.println("key2 : " + x2 + " , value2 : " + y2);
+                    });
+                });
+                
+//                JsonElement json = gson.fromJson(dataSnapshot.getValue(), JsonElement.class);
+//                String result = gsons.toJson(dataSnapshot.getValue());
+//                System.out.println(result);
+//                gsons.fromJson(dataSnapshot.getValue());
+//                Gson gsons = dataSnapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
+        
+        
+//        System.out.println(text);        // 「hello」と出力
         
 //        Object user = {"kode","ase"};
-//        ref.child("fhaiohrlaljkdad").child("komagiregmaicom").setValue("papapa");
-        
-        
-        
+//        ref.child("user1").child("email").setValue("komagire@gmai.com");
+//        ref.child("user1").child("pass").setValue("papapa");
+//        
+//        ref.child("user2").child("email").setValue("mousiwakenai@gmai.com");
+//        ref.child("user2").child("pass").setValue("ooo");
+//        
+//        ref.child("user3").child("email").setValue("kokokokok@gmai.com");
+//        ref.child("user3").child("pass").setValue("dafae");
+//        
+//        ref.child("user4").child("email").setValue("wajoiyr@gmai.com");
+//        ref.child("user4").child("pass").setValue("dafae");
+//        
+//        ref.child("user5").child("email").setValue("loiyd@gmai.com");
+//        ref.child("user5").child("pass").setValue("dafae");
         
         
 //        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
